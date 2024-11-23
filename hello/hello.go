39f28d97cf4df9d24d14a95baf508829468f4adf
Mike Miroliubov <mike.miroliubov@klaviyo.com>
2024-11-23 10:47:17 -0500

    Refactored to use tests

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,9 @@
 package hello
 
-import "fmt"
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+const helloPrefix = "Hello World, "
 
 func Hello(name string) string {
-	return "Hello World, " + name
-}
-
-func main() {
-	fmt.Println(Hello("User"))
+	return helloPrefix + name
 }
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "learn-go-with-tests/hello"
+
+func main() {
+	fmt.Println(hello.Hello("User"))
+}
