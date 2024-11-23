70fd8854bfc2c2d9bd050f301a776dc9b4f02917
Mikhail Miroliubov <mike.miroliubov@gmail.com>
2024-11-23 18:14:17 +0000

    Added multiple tests

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,11 @@ package hello
 
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-const helloPrefix = "Hello World, "
+const helloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		return helloPrefix + "World"
+	}
 	return helloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,12 +3,25 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	// when
-	result := Hello("TestUser")
+	t.Run("Say hello when no name is provided", func(t *testing.T) {
+		// when
+		result := Hello("")
 
-	// then
-	expected := "Hello World, TestUser"
-	if result != expected {
-		t.Errorf("Expected %q, got %q", expected, result)
-	}
+		// then
+		expected := "Hello, World"
+		if result != expected {
+			t.Errorf("Expected %q, got %q", expected, result)
+		}
+	})
+
+	t.Run("Say hello to a user by Name", func(t *testing.T) {
+		// when
+		result := Hello("TestUser")
+
+		// then
+		expected := "Hello, TestUser"
+		if result != expected {
+			t.Errorf("Expected %q, got %q", expected, result)
+		}
+	})
 }
