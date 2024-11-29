142411f5d7c7015beb0dc1141f3c61151fafacba
Mike Miroliubov <mike.miroliubov@klaviyo.com>
2024-11-29 13:23:06 -0500

    Reflection - table-driven test

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,6 +3,28 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	fn(val.Field(0).String())
+
+	if val.Kind() == reflect.Pointer {
+		return val.Elem()
+	}
+
+	return val
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	// given
 	cases := []struct {
@@ -19,16 +29,58 @@ func TestWalk(t *testing.T) {
 			}{"Chris"},
 			[]string{"Chris"},
 		},
+		{
+			"struct with 2 string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non sting field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"struct with nested fields",
+			Person{
+				Name:    "Chris",
+				Profile: Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"pointer to struct",
+			&Person{
+				Name:    "Chris",
+				Profile: Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{Age: 33, City: "London"},
+				{Age: 34, City: "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 
 	for _, test := range cases {
-		// when
-		var calls []string
-		walk(test.input, func(input string) {
-			calls = append(calls, input)
+		t.Run(test.name, func(t *testing.T) {
+			// when
+			var calls []string
+			walk(test.input, func(input string) {
+				calls = append(calls, input)
+			})
+			// then
+			assert.Equal(t, test.expectedCalls, calls)
 		})
-		// then
-		assert.Equal(t, test.expectedCalls, calls)
 	}
 
 }
